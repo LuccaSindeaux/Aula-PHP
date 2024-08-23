cdb76a24fd173fec73a86e82eda3626c1ffdcac4
Lucca Sindeaux <93556947+LuccaSindeaux@users.noreply.github.com>
2024-08-23 09:45:23 -0300

    Add files via upload

---
+++ aula-1.php
@@ -0,0 +1,17 @@
+<?php 
+//Variáveis com PHP
+//Nome de variáveis tendem a ser separados com letras maisúculas ou underline: $nomeCompleto $nome_completo
+$aluno = 'Lucca Sindeaux'; //Precisa terminar tudo com ;
+$idade = 26; //int
+$peso = 82.3; //float
+$aceite = true; //valores boleanos de verdadeiro ou falso (true/false)
+
+$x = 10;
+$y = 5;
+$soma = $x + $y;
+
+echo "O aluno ".$aluno." está com ".$idade." anos."; //para mostrar na tela
+echo '<br>';
+echo $soma;
+
+?>
